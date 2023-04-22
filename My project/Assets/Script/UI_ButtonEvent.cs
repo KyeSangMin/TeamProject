1385083e0cf607704bae08599b31b4fd32877d80
KyeSangMin <37332140+KyeSangMin@users.noreply.github.com>
2023-04-23 01:11:45 +0900

    2023/04/23 배경 스크롤링 구현
    배경 오토 스크롤 기능 구현

--- My project/Assets/Script/UI_ButtonEvent.cs
+++ My project/Assets/Script/UI_ButtonEvent.cs
@@ -7,13 +7,17 @@ public class UI_ButtonEvent : MonoBehaviour
 
     GameObject Puzzle_UI;
     GameObject PAD_UI;
-
+    GameObject Dialogue_UI;
 
     // Start is called before the first frame update
     void Start()
     {
         Puzzle_UI = GameObject.Find("UI_Puzzle");
         PAD_UI = GameObject.Find("UI_SlidePad");
+        Dialogue_UI = GameObject.Find("UI_Dialogue");
+        Puzzle_UI.SetActive(false);
+        PAD_UI.SetActive(false);
+        Dialogue_UI.SetActive(false);
     }
 
     // Update is called once per frame
@@ -51,7 +55,20 @@ public class UI_ButtonEvent : MonoBehaviour
         }
     }
 
+    public void SetDialogueActive()
+    {
 
+        if (Dialogue_UI.activeSelf == true)
+        {
+            Dialogue_UI.SetActive(false);
+        }
+        else
+        {
+            Dialogue_UI.SetActive(true);
+        }
+
+
+    }
 
 
 
